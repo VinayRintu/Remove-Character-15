3cb87be86604803e600791bb70da4ffe2b64aadd
Rintu <Rintu@VINDUJA>
2022-10-31 16:47:08 +0530

    Add project files.

---
+++ Remove Character 15/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remove_Character_15
+{
+    internal class Program
+    {
+       public static string removechar(string abc, int n)
+        {
+            return abc.Remove(n, 1);
+        } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(removechar("vinayrintu", 4));
+            Console.WriteLine(removechar("ravula", 4));
+            Console.WriteLine(removechar("pawankalyan", 1));
+            Console.ReadLine();
+        }
+    }
+}
